c6a98a5d4ce2dcc73b97a9fa2d9925c03cb3a822
joshualoui <joshua.louie@edu.sait.ca>
2025-02-09 23:13:14 -0700

    First Commit
    Working on Sign in Page

---
+++ app/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, View, Text } from "react-native";
+import SignIn from "../components/signin";
+import { useState } from "react";
+
+export default function App() {
+  const [username, setUsername] = useState<string>("");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false); // default is false
+  //when isLoggedIn is toggled to True then LandingPage is displayed.
+  return (
+    <View>
+      <Text>
+        Sign in and landing page. Need to surround SignIn and landingpage with
+        conditional statement. When loggedin=True is swaps to LandingPage.
+        Default is sign in page.
+      </Text>
+
+      <SignIn
+        username={username}
+        booleanToggle={setIsLoggedIn}
+        setUsername={setUsername}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+    fontSize: 20,
+  },
+});
---
+++ components/signin.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import credentials from "../credentials.json";
+import { Link, useRouter } from "expo-router";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+
+type SignInProps = {
+  booleanToggle: (isLoggedIn: boolean) => void;
+  username: string;
+  setUsername: (username: string) => void;
+};
+
+const SignIn: React.FC<SignInProps> = ({
+  booleanToggle,
+  username,
+  setUsername,
+}) => {
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const handleSubmit = () => {
+    const user = credentials.users.find(
+      (user) => user.username === username && user.password === password
+    );
+    if (user) {
+      booleanToggle(true);
+    } else {
+      alert("Invalid username or password");
+    }
+  };
+
+  return (
+    <View>
+      <Text>Sign In</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={handleSubmit} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+    fontSize: 20,
+  },
+});
+export default SignIn;
